8aad30e657d88ae30edde2ad019104408a2bc549
jaiderarevalo <jaiderruizx12@gmail.com>
2024-03-05 00:29:06 -0500

    register user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { UserService } from '@/user/user.service';
+import { Auth } from './decorators/auth.decorator';
+import { Role } from '@/interfaces/role.enum';
+import { CreateUserDto } from '@/user/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
+  ) {}
 
+  /*   @Post('create')
+  @HttpCode(200)
+  async login (@Body login) */
+
+  @Post('create')
+  @HttpCode(200)
+  @Auth(Role.User)
+  async create(@Body() CreateuserDto: CreateUserDto) {
+    console.log('lo que llega De dto', CreateuserDto);
+
+    if (CreateuserDto.password !== CreateuserDto.confirmPassword) {
+      throw new HttpException(
+        'Las contraseñas no coinciden',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+   
+    const user = await this.userService.create(CreateuserDto);
+    const { email, name, role } = user as any;
+    return { email, name, role };
+  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -11,7 +12,23 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
   async create(createUserDto: CreateUserDto) {
-    return console.log(createUserDto);
-    
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const res = this.userRepository.save({
+        email: createUserDto.email,
+        name: createUserDto.name,
+        password: await bcrypt.hash(createUserDto.password, salt),
+        role: createUserDto.role,
+      });
+      if (!res) {
+        throw new HttpException(`${res}`, HttpStatus.BAD_REQUEST);
+      }
+      return res;
+    } catch (error) {
+      throw new HttpException(
+        `${error.message}`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
