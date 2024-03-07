09d46204b129e4b1f3a66acd06501489613de0e0
jaiderarevalo <jaiderruizx12@gmail.com>
2024-03-06 22:25:39 -0500

    feat/rolesArrays

--- src/auth/guard/roles.guard.ts
+++ src/auth/guard/roles.guard.ts
@@ -7,14 +7,14 @@ import { ROLES_KEY } from '../decorators/roles.decorator';
 export class RolesGuard implements CanActivate {
   constructor(private readonly reflector: Reflector) {}
   canActivate(context: ExecutionContext): boolean {
-    const role = this.reflector.getAllAndOverride<Role>(ROLES_KEY, [
+    const roles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
-    if (!role) {
+    if (!roles) {
       return true;
     }
     const { user } = context.switchToHttp().getRequest();
-    return role === user.role;
+    return roles.includes(user.role);
   }
 }
